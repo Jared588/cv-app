30572bd2b71977720c63b6e40017fd5ae7c109b4
Jared588 <jaredeprice@gmail.com>
2024-02-05 16:53:19 +0200

    Make inputs collapsable

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,87 +1,117 @@
+import { useState } from 'react';
 import InputContainer from './containers/InputContainer';
 import PropTypes from 'prop-types';
 import './input.css';
 
 export default function Info({ handleChange }) {
-    
+    const [isContainerVisible, setContainerVisibility] = useState(false);
+    const toggleContainerVisbility = () => {
+        setContainerVisibility(!isContainerVisible)
+    }
     return  (
         <div>
             {/* Personal Details */}
-            <h1 className='input-heading'>Personal Details</h1>
+            <h1 className='input-heading' onClick={toggleContainerVisbility}>Personal Details</h1>
+            {isContainerVisible && (
                 <div className="infoContainer" id='personal'>
-                <InputContainer title='Full Name' id='fullName' type="text" onChange={handleChange}/>
-                <InputContainer title='Job Title* (optional)' id='title' type="text" onChange={handleChange}/>
-                <InputContainer title='Email' id='email' type="email" onChange={handleChange}/>
-                <InputContainer title='Phone Number' id='phoneNumber' type="tel" onChange={handleChange}/>
-                <InputContainer title='Address' id='address' type="text" onChange={handleChange}/>
-                <div className="inputContainer">
-                    <label htmlFor="aboutme">About me</label>
-                    <textarea name="aboutme" id="aboutme" cols="30" rows="5" className='textarea' onChange={(e) => handleChange('aboutme', e.target.value)}></textarea>
+                    <InputContainer title='Full Name' id='fullName' type="text" onChange={handleChange}/>
+                    <InputContainer title='Job Title* (optional)' id='title' type="text" onChange={handleChange}/>
+                    <InputContainer title='Email' id='email' type="email" onChange={handleChange}/>
+                    <InputContainer title='Phone Number' id='phoneNumber' type="tel" onChange={handleChange}/>
+                    <InputContainer title='Address' id='address' type="text" onChange={handleChange}/>
+                    <div className="inputContainer">
+                        <label htmlFor="aboutme">About me</label>
+                        <textarea name="aboutme" id="aboutme" cols="30" rows="5" className='textarea' onChange={(e) => handleChange('aboutme', e.target.value)}></textarea>
+                    </div>
                 </div>
-            </div> 
+            )}
         </div>
     )
 }
 
 export function Education({ handleEducationForm, handleAddEducation, formData }) {
+    const [isContainerVisible, setContainerVisibility] = useState(false);
+    const toggleContainerVisbility = () => {
+        setContainerVisibility(!isContainerVisible)
+    }
     return ( 
         <>
-            <h1 className='input-heading'>Education</h1>
-            <div className="infoContainer" id='education'>
-                <InputContainer title='School' id='school' type="text" value={formData.school || ''} onChange={handleEducationForm}/>
-                <InputContainer title='Degree' id='degree' type="text" value={formData.degree || ''} onChange={handleEducationForm}/>
-                <div className='dateContainer'>
-                    <InputContainer title='Start Date' id='eduStartDate' type="date" value={formData.startDate || ''} onChange={handleEducationForm}/>
-                    <InputContainer title='End Date' id='eduEndDate' type="date" value={formData.endDate || ''} onChange={handleEducationForm}/>
+            <h1 className='input-heading' onClick={toggleContainerVisbility}>Education</h1>
+            {isContainerVisible && (
+                <div className="infoContainer" id='education'>
+                    <InputContainer title='School' id='school' type="text" value={formData.school || ''} onChange={handleEducationForm}/>
+                    <InputContainer title='Degree' id='degree' type="text" value={formData.degree || ''} onChange={handleEducationForm}/>
+                    <div className='dateContainer'>
+                        <InputContainer title='Start Date' id='eduStartDate' type="date" value={formData.startDate || ''} onChange={handleEducationForm}/>
+                        <InputContainer title='End Date' id='eduEndDate' type="date" value={formData.endDate || ''} onChange={handleEducationForm}/>
+                    </div>
+                    <button className='addBtn' id='eduBtn' onClick={() => handleAddEducation(formData)}>Add +</button>
                 </div>
-                <button className='addBtn' id='eduBtn' onClick={() => handleAddEducation(formData)}>Add +</button>
-            </div>
+            )}
         </>
     )
 }
 
 export function Experience({ handleExpForm, handleAddExp, expData }) {
+    const [isContainerVisible, setContainerVisibility] = useState(false);
+    const toggleContainerVisbility = () => {
+        setContainerVisibility(!isContainerVisible)
+    }
     return (
         <>
-            <h1 className='input-heading'>Experience</h1>
-            <div className="infoContainer" id='experience'>
-                <InputContainer title='Company Name' id='company' type="text" value={expData.company || ''} onChange={handleExpForm}/>
-                <InputContainer title='Position Title' id='position' type="text" value={expData.position || ''} onChange={handleExpForm}/>
-                <div className='dateContainer'>
-                    <InputContainer title='Start Date' id='expStartDate' type="date" value={expData.expStartDate || ''} onChange={handleExpForm}/>
-                    <InputContainer title='End Date' id='expEndDate' type="date" value={expData.expEndDate || ''} onChange={handleExpForm}/>
+            <h1 className='input-heading' onClick={toggleContainerVisbility}>Experience</h1>
+            {isContainerVisible && (
+                <div className="infoContainer" id='experience'>
+                    <InputContainer title='Company Name' id='company' type="text" value={expData.company || ''} onChange={handleExpForm}/>
+                    <InputContainer title='Position Title' id='position' type="text" value={expData.position || ''} onChange={handleExpForm}/>
+                    <div className='dateContainer'>
+                        <InputContainer title='Start Date' id='expStartDate' type="date" value={expData.expStartDate || ''} onChange={handleExpForm}/>
+                        <InputContainer title='End Date' id='expEndDate' type="date" value={expData.expEndDate || ''} onChange={handleExpForm}/>
+                    </div>
+                    <InputContainer title='Location' id='compLocation' type="text" value={expData.compLocation || ''} onChange={handleExpForm}/>
+                    <div className="inputContainer">
+                        <label htmlFor="description">Description</label>
+                        <textarea name="description" id="description" cols="30" rows="5" className='textarea' value={expData.description || ''} onChange={(e) => handleExpForm('description', e.target.value)}></textarea>
+                    </div>
+                    <button className='addBtn' id='expBtn' onClick={() => handleAddExp(expData)}>Add +</button>
                 </div>
-                <InputContainer title='Location' id='compLocation' type="text" value={expData.compLocation || ''} onChange={handleExpForm}/>
-                <div className="inputContainer">
-                    <label htmlFor="description">Description</label>
-                    <textarea name="description" id="description" cols="30" rows="5" className='textarea' value={expData.description || ''} onChange={(e) => handleExpForm('description', e.target.value)}></textarea>
-                </div>
-                <button className='addBtn' id='expBtn' onClick={() => handleAddExp(expData)}>Add +</button>
-            </div>
+            )}
         </>
     )
 }
 
 export function Skills({ handleAddSkill, skillValue, setSkillValue }) {
+    const [isContainerVisible, setContainerVisibility] = useState(false);
+    const toggleContainerVisbility = () => {
+        setContainerVisibility(!isContainerVisible)
+    }
     return (
         <>
-            <h1 className='input-heading'>Skills</h1>
-            <div className='infoContainer'>
-                <input type="text" id='skill' value={skillValue} onChange={(e) => setSkillValue(e.target.value)}/>
-                <button className='addBtn' id='skillBtn' onClick={() => handleAddSkill(skillValue)}>Add +</button>
-            </div>
+            <h1 className='input-heading' onClick={toggleContainerVisbility}>Skills</h1>
+            {isContainerVisible && (
+                <div className='infoContainer'>
+                    <input type="text" id='skill' value={skillValue} onChange={(e) => setSkillValue(e.target.value)}/>
+                    <button className='addBtn' id='skillBtn' onClick={() => handleAddSkill(skillValue)}>Add +</button>
+                </div>
+            )}
         </>
     )
 }
 
 export function Languages({ handleAddLang, langValue, setLangValue }) {
+    const [isContainerVisible, setContainerVisibility] = useState(false);
+    const toggleContainerVisbility = () => {
+        setContainerVisibility(!isContainerVisible)
+    }
     return (
         <>
-            <h1 className='input-heading'>Languages</h1>
-            <div className='infoContainer'>
-                <input type="text" id='language' value={langValue} onChange={(e) => setLangValue(e.target.value)}/>
-                <button className='addBtn' id='langBtn' onClick={() => handleAddLang(langValue)}>Add +</button>
-            </div>
+            <h1 className='input-heading' onClick={toggleContainerVisbility}>Languages</h1>
+            {isContainerVisible && (
+                <div className='infoContainer'>
+                    <input type="text" id='language' value={langValue} onChange={(e) => setLangValue(e.target.value)}/>
+                    <button className='addBtn' id='langBtn' onClick={() => handleAddLang(langValue)}>Add +</button>
+                </div>
+            )}
         </>
     )
 }
